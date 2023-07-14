1ddf92b6a8878196da0e44bc8231e81c0750522c
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-07-14 16:10:35 +0600

    Initial setup

---
+++ components/Map.js
@@ -0,0 +1,17 @@
+import Map from 'react-map-gl';
+
+
+function App() {
+    return (
+        <Map
+            mapboxAccessToken="<Mapbox access token>"
+            initialViewState={{
+                longitude: -122.4,
+                latitude: 37.8,
+                zoom: 14
+            }}
+            style={{ width: 600, height: 400 }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
+    );
+}
